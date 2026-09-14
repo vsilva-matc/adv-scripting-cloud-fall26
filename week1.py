58762c48b796bada89da5ac3703de19df6e10ac1
Virginia Potestivo <vsilva@madisoncollege.edu>
2026-09-14 16:01:36 -0500

    more changes

--- week1.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-sts = boto3.client('sts')
-hFile = open('credential-check.txt', 'w')
-result = sts.get_caller_identity()
-print(str(result))
-hFile.write(str(result))
-hFile.close()
-
